64057b30f2381198ddede12c0d6eaa96ce0aed22
Binit Datta <binitdatta@gmail.com>
2018-05-14 20:31:15 -0500

    First Commit

---
+++ src/main/java/com/rollingstone/RollingstoneAirlineApiSpringJavaConfigApplication.java
@@ -0,0 +1,12 @@
+package com.rollingstone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RollingstoneAirlineApiSpringJavaConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RollingstoneAirlineApiSpringJavaConfigApplication.class, args);
+	}
+}
